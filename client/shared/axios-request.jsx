cfbb68c7aafe00d26a7e274683482a1a5521e291
Ngela <ngela@Ngela-Pat-Yu.local>
2018-09-07 06:23:08 +0200

    axios-request - Manage the Axios request

---
+++ client/shared/axios-request.jsx
@@ -0,0 +1,34 @@
+
+import axios from 'axios';
+import apiUrl from './constants';
+
+
+/**
+ * Axios Request, onSuccess, onError 
+ */
+const request = function (options) {
+    const onSuccess = function (response) {
+        // TODO: Add success alert?
+        return response.data;
+    }
+
+    const onError = function (error) {
+        if (error.response) {
+            // TODO: Add error alert error.config, error.response.status
+            // It's not a 200 status
+        } else {
+            // TODO: Add error alert  error.message
+            // Request coudn't be processed
+            console.error('Error Message:', error.message);
+        }
+        return Promise.reject(error.response || error.message);
+    }
+
+    return axios.create({
+        baseURL: apiUrl
+    })(options)
+        .then(onSuccess)
+        .catch(onError);
+}
+
+export default request;
